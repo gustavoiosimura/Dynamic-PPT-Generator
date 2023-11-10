4bcc6490c101cabcaa3a32f2231cc3361ddf1cdf
Gustavo Iosimura <guiosimura@hotmail.com>
2023-11-10 08:16:36 -0300

    decodes a base64 pptx file

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
-const express = require('express'); 
+const express = require('express');
+const JSZip = require('jszip');
 
 const app = express();
-  
+ 
+app.use(express.json({limit: '50mb'}));
 const port = 3000;
 
 app.get('/', (req, res) => {
@@ -11,8 +13,24 @@ app.get('/', (req, res) => {
 
 
 app.post('/', async (req, res) => {
+    const data = req.body;  
 
-    
+    // Decode the base64 string to a Buffer
+    const pptxBuffer = Buffer.from(data.pptx, 'base64');
+
+    // Creates a JSZip instance to work with the PowerPoint file
+    const zip = new JSZip();
+    await zip.loadAsync(pptxBuffer);
+
+    // Find and replace #token# in all XML files inside the PPTX
+    const files = zip.files;
+    for (const file in files) {
+        if (file.endsWith('.xml')) {
+            const content = await files[file].async('string');
+            let updatedContent = content.replace(/#token#/g, `my-var`); 
+            zip.file(file, updatedContent);
+        }
+    }
 
     res.json({ message: 'Data received successfully', receivedData: data });
 });
